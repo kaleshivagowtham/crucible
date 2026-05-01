03c3463cd69615c79b7a735241d635fc1aacc673
Shiva_Gowtham_Kale <kaleshivagowtham@gmail.com>
2026-05-01 15:52:09 +0530

    Functional chain
    - writer chain for writing the reports
    - critic chain for analyzing and pointing to improvements
    - search agent for searching the web
    - reader agent to summarize the web content
    - beafutiful soup to extract the content off the website

--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@ from langchain_community.tools import tool
 import requests
 from bs4 import BeautifulSoup
 from tavily import TavilyClient
+from rich import print
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,8 +12,27 @@ tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 @tool
 def web_search(query: str) -> str:
     """Search the web for recent and relatable information on a topic. Returns Titles, Urls and Snippets."""
-    results = tavily.search(query=query, max_results=5)
+    results = tavily.search(query=query, max_results=1)
 
-    return results
+    out = []
 
-print(web_search.invoke("What are the recent news of war?"))
+    for r in results['results']:
+        out.append(
+            f"Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'][:300]}\n"
+        )
+    print("inside web_search tool", out)
+    return "\n-----\n".join(out)
+
+@tool
+def scrape_url(url: str) -> str:
+    """Scrape and return clean text content form given URL for deeper reading."""
+    try:
+        resp = requests.get(url, timeout=8, headers={"User-Agent": "Mozilla/5.0"})
+        soup = BeautifulSoup(resp.text, "html.parser")
+        for tag in soup(["script", "style", "nav", "footer"]):
+            tag.decompose()
+        return soup.get_text(separator=" ", strip=True)[:3000]
+    except Exception as e:
+        return f"Could not scrape URL: {str(e)}"
+    
+# print(web_search.invoke("What is impact of war on stock market?"))
